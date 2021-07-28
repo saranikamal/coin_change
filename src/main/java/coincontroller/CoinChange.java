0f3a3342b3fbf2fa9795905b82030395fd963ef1
Sara NikAmalFard <sarah_nikamalfard@yahoo.com>
2021-07-28 23:51:12 +0100

    Added program logic to coin controller.

--- src/main/java/coincontroller/CoinChange.java
+++ src/main/java/coincontroller/CoinChange.java
@@ -1,14 +1,23 @@
 package coincontroller;
 
+import utility.Algorithm;
+import utility.Greedy;
 import utility.InputOutput;
+
+import java.util.List;
 
 public class CoinChange {
     private final InputOutput inputOutput = new InputOutput();
+    private Algorithm algo = new Greedy();
 
 
     public void runProgram() {
+        int input;
+        List<Integer> minimumNumberOfCoins;
         while (true) {
-            inputOutput.showOutput();
+            input = inputOutput.getInput();
+            minimumNumberOfCoins = algo.getMinimumNumberOfCoins(input);
+            inputOutput.showOutput(minimumNumberOfCoins, algo.getCoinList());
         }
     }
 }
