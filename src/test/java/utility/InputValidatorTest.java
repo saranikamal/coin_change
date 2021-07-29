d1ae6339bfa88675bcf10fa9d85a6f8d2856d2ee
Sara NikAmalFard <sarah_nikamalfard@yahoo.com>
2021-07-29 14:45:26 +0100

    Test cases to test the functionality of isInputValid method.

---
+++ src/test/java/utility/InputValidatorTest.java
@@ -0,0 +1,72 @@
+package utility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputValidatorTest {
+      private Validator inputValidator = new InputValidator();
+
+    @Test
+    void inputWithPoundSignOnly() {
+        String input = "£2";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertTrue(isValid);
+    }
+
+    @Test
+    void inputWithPenceSignOnly() {
+        String input = "2p";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertTrue(isValid);
+    }
+
+    @Test
+    void inputWithPenceAndPoundSigns() {
+        String input = "£23p";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertTrue(isValid);
+    }
+
+    @Test
+    void floatingPointInputWithDotAtBeginning() {
+        String input = ".9";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertTrue(isValid);
+    }
+
+    @Test
+    void floatingPointInputWithDotAtTheEnd() {
+        String input = "9.";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertFalse(isValid);
+    }
+
+    @Test
+    void inputMissingDigits() {
+        String input = "£p";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertFalse(isValid);
+    }
+
+    @Test
+    void wrongCharactersInInput() {
+        String input = "1x";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertFalse(isValid);
+    }
+
+    @Test
+    void validSymbolsAndNumericWithWrongSymbol() {
+        String input = "£1x.0p";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertFalse(isValid);
+    }
+
+    @Test
+    void emptyInput() {
+        String input = " ";
+        boolean isValid = inputValidator.isInputValid(input);
+        assertFalse(isValid);
+    }
+}
